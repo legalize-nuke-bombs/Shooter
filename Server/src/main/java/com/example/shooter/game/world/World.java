f0b8899e18599d109e286a96cd81e599a7d6f21d
legalize-nuke-bombs <cheb0tarev.nikit0s@gmail.com>
2026-07-15 17:58:57 +0300

    refactor

--- Server/src/main/java/com/example/shooter/game/world/World.java
+++ Server/src/main/java/com/example/shooter/game/world/World.java
@@ -1,17 +1,13 @@
 package com.example.shooter.game.world;
 
-import com.example.shooter.user.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Table(name = "worlds", indexes = {
         @Index(name = "idx_worlds_name", columnList = "name"),
-        @Index(name = "idx_worlds_created_at", columnList = "created_at"),
-        @Index(name = "idx_worlds_creator_id", columnList = "creator_id")
+        @Index(name = "idx_worlds_created_at", columnList = "created_at")
 })
 @Getter
 @Setter
@@ -25,11 +21,6 @@ public class World {
 
     @Column(nullable = false)
     private Long createdAt;
-
-    @ManyToOne
-    @JoinColumn(name = "creator_id", nullable = false)
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    private User creator;
 
     @Column(nullable = false, length = WorldConstants.MAX_DESCRIPTION_LENGTH)
     private String description;
