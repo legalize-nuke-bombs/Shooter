8a6a8938187f9f0ac59dc383f116d11ffd77cf13
legalize-nuke-bombs <cheb0tarev.nikit0s@gmail.com>
2026-07-15 17:55:45 +0300

    add player entity

---
+++ Server/src/main/java/com/example/shooter/game/player/Player.java
@@ -0,0 +1,46 @@
+package com.example.shooter.game.player;
+
+import com.example.shooter.game.util.AccessLevel;
+import com.example.shooter.game.util.Vector3d;
+import com.example.shooter.game.world.World;
+import com.example.shooter.user.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table(name = "players", indexes = {
+        @Index(name = "idx_players_world_id", columnList = "world_id"),
+        @Index(name = "idx_players_user_id", columnList = "user_id")
+}, uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"world_id", "user_id"})
+})
+@Getter
+@Setter
+public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "world_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private World world;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AccessLevel accessLevel;
+
+    @Column(nullable = false)
+    private Long memberSince;
+
+    @Column(nullable = false)
+    private Vector3d position;
+}
