7c8b0a9d4e432d67510b0b91b550b06fba6f7d78
legalize-nuke-bombs <cheb0tarev.nikit0s@gmail.com>
2026-07-15 18:00:54 +0300

    bug fix

--- Server/src/main/java/com/example/shooter/game/util/Vector3d.java
+++ Server/src/main/java/com/example/shooter/game/util/Vector3d.java
@@ -1,5 +1,6 @@
 package com.example.shooter.game.util;
 
+import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Embeddable
 public class Vector3d {
     private double x, y, z;
 }
