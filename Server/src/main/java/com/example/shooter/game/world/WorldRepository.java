35ea3da4fc57b8b85ad16e939da3064603b7731e
legalize-nuke-bombs <cheb0tarev.nikit0s@gmail.com>
2026-07-15 23:57:48 +0300

    minor code cleanup

--- Server/src/main/java/com/example/shooter/game/world/WorldRepository.java
+++ Server/src/main/java/com/example/shooter/game/world/WorldRepository.java
@@ -18,12 +18,6 @@ public interface WorldRepository extends JpaRepository<World, UUID> {
     @Query("SELECT COUNT(w) FROM World w WHERE w.accessedAt >= ?1")
     long countAccessedSince(Long timestamp);
 
-    @Query("SELECT w FROM World w WHERE (?1 IS NULL OR w.id in ?1) AND (?2 IS NULL OR w.visibilityPolicy = ?2) AND (?3 IS NULL OR w.joinPolicy = ?3) ORDER BY w.name, w.id")
-    List<World> findByWorldIdsAndVisibilityPolicyNameOrder(Set<UUID> worldIds, WorldVisibilityPolicy visibilityPolicy, WorldJoinPolicy joinPolicy, Pageable pageable);
-
-    @Query("SELECT w FROM World w WHERE (?1 IS NULL OR w.id in ?1) AND (?2 IS NULL OR w.visibilityPolicy = ?2) AND (?3 IS NULL OR w.joinPolicy = ?3) ORDER BY w.createdAt DESC, w.id")
-    List<World> findByWorldIdsAndVisibilityPolicyCreatedAtOrder(Set<UUID> worldIds, WorldVisibilityPolicy visibilityPolicy, WorldJoinPolicy joinPolicy, Pageable pageable);
-
     @Query("SELECT w FROM World w WHERE (?1 IS NULL OR w.id in ?1) AND (?2 IS NULL OR w.visibilityPolicy = ?2) AND (?3 IS NULL OR w.joinPolicy = ?3) ORDER BY w.accessedAt DESC, w.id")
     List<World> findByWorldIdsAndVisibilityPolicyAccessedAtOrder(Set<UUID> worldIds, WorldVisibilityPolicy visibilityPolicy, WorldJoinPolicy joinPolicy, Pageable pageable);
 
