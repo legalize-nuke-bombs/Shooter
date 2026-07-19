ded8a87f7cc41fcc61f7ef00a34f855d0177677a
legalize-nuke-bombs <cheb0tarev.nikit0s@gmail.com>
2026-07-20 01:43:33 +0300

    move java server metadata to application.properties

--- Server/src/main/java/com/example/shooter/server/ServerConfig.java
+++ Server/src/main/java/com/example/shooter/server/ServerConfig.java
@@ -1,18 +1,25 @@
 package com.example.shooter.server;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ServerConfig {
 
+    private final ServerRepresentation serverRepresentation;
+
+    public ServerConfig(@Value("${server.name}") String name, @Value("${server.major}") int major, @Value("${server.minor}") int minor, @Value("${server.patch}") int patch) {
+        this.serverRepresentation = new ServerRepresentation(
+                name,
+                major,
+                minor,
+                patch
+        );
+    }
+
     @Bean
     public ServerRepresentation serverRepresentation() {
-        return new ServerRepresentation(
-            "Shooter Server Main",
-                0,
-                0,
-                2
-        );
+        return serverRepresentation;
     }
 }
