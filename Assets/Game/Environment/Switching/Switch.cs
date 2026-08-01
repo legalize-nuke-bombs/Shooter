db1673354dc6826b4912dfaef5e93d2a49bdebbe
mr2anderson <cheb0tarev.nikit0s@gmail.com>
2026-08-01 16:46:23 +0300

    Выключатели щёлкают: звук на двенадцать метров с линейным затуханием

--- Assets/Game/Environment/Switching/Switch.cs
+++ Assets/Game/Environment/Switching/Switch.cs
@@ -1,10 +1,12 @@
 using Shooter.Game.Body;
+using Shooter.Game.Body.Sounding;
 using Shooter.Logging;
 using Unity.Netcode;
 using UnityEngine;
 
 namespace Shooter.Game
 {
+    [RequireComponent(typeof(Speaker))]
     public class Switch : NetworkBehaviour, IUsable, IDigestible
     {
         private static readonly Journal Log = Logs.Here();
@@ -17,14 +19,19 @@ namespace Shooter.Game
 
         [SerializeField] private Renderer[] glowing;
 
+        [SerializeField] private SoundSpec click;
+
         private readonly NetworkVariable<bool> shining = new NetworkVariable<bool>(true);
 
         private Color[] glows;
+
+        private Speaker speaker;
 
         public UsageType Usage => shining.Value ? UsageType.TurnOff : UsageType.TurnOn;
 
         private void Awake()
         {
+            speaker = GetComponent<Speaker>();
             glows = new Color[glowing.Length];
 
             for (int i = 0; i < glowing.Length; i++)
@@ -47,6 +54,7 @@ namespace Shooter.Game
         public void Use(NetworkObject user)
         {
             shining.Value = !shining.Value;
+            speaker.Play(click);
             Log.Info("Entity {} switched {} {}", user.name, name, shining.Value ? "on" : "off");
         }
 
