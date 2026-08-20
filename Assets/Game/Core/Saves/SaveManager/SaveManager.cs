6be7cbfc60b62ad4594e671ed4c063a550c5251b
mr2anderson <cheb0tarev.nikit0s@gmail.com>
2026-08-20 16:02:47 +0300

    drop dead using

--- Assets/Game/Core/Saves/SaveManager/SaveManager.cs
+++ Assets/Game/Core/Saves/SaveManager/SaveManager.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.IO;
-using System.IO.Compression;
 using Shooter.Configuring;
 using Shooter.Logging;
 using UnityEngine;
