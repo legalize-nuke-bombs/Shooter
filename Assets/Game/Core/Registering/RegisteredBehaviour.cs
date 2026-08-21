76b4533f5ea8bb09e4c1946e70a2b025b5ebeac8
mr2anderson <cheb0tarev.nikit0s@gmail.com>
2026-08-21 20:17:17 +0300

    proper menu 3d map, RegisterBehaviour patches

--- Assets/Game/Core/Registering/RegisteredBehaviour.cs
+++ Assets/Game/Core/Registering/RegisteredBehaviour.cs
@@ -1,18 +1,34 @@
+using Shooter.Logging;
 using UnityEngine;
 
 namespace Shooter.Game.Core
 {
     public abstract class RegisteredBehaviour : MonoBehaviour
     {
+        private static readonly Journal Log = Logs.Here();
+
         protected virtual void OnEnable()
         {
+            Registers registers = Registers.Current;
+            if (registers == null)
+            {
+                Log.Info($"Entity {name} will not be tracked because registers is not set");
+                return;
+            }
+            Log.Info($"Entity {name} is tracking");
             Registers.Current.Track(this);
         }
 
         protected virtual void OnDisable()
         {
-            Registers world = Registers.Current;
-            if (world != null) world.Untrack(this);
+            Registers registers = Registers.Current;
+            if (registers == null)
+            {
+                Log.Info($"Entity {name} will not be untracked because registers is not set");
+                return;
+            }
+            Log.Info($"Entity {name} is untracking");
+            Registers.Current.Untrack(this);
         }
     }
 }
--- Assets/Game/Core/Registering/RegisteredNetworkBehaviour.cs
+++ Assets/Game/Core/Registering/RegisteredNetworkBehaviour.cs
@@ -1,18 +1,34 @@
+using Shooter.Logging;
 using Unity.Netcode;
 
 namespace Shooter.Game.Core
 {
     public abstract class RegisteredNetworkBehaviour : NetworkBehaviour
     {
+        private static readonly Journal Log = Logs.Here();
+
         public override void OnNetworkSpawn()
         {
+            Registers registers = Registers.Current;
+            if (registers == null)
+            {
+                Log.Info($"Entity {name} will not be tracked because registers is not set");
+                return;
+            }
+            Log.Info($"Entity {name} is tracking");
             Registers.Current.Track(this);
         }
 
         public override void OnNetworkDespawn()
         {
-            Registers world = Registers.Current;
-            if (world != null) world.Untrack(this);
+            Registers registers = Registers.Current;
+            if (registers == null)
+            {
+                Log.Info($"Entity {name} will not be untracked because registers is not set");
+                return;
+            }
+            Log.Info($"Entity {name} is untracking");
+            Registers.Current.Untrack(this);
         }
     }
 }
