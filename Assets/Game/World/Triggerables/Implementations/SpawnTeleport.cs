36ffafde08b5fd2bfd8b7e6e1176220a4dbdcc8f
mr2anderson <cheb0tarev.nikit0s@gmail.com>
2026-08-24 17:57:47 +0300

    add corridors dim prototype

---
+++ Assets/Game/World/Triggerables/Implementations/SpawnTeleport.cs
@@ -0,0 +1,30 @@
+﻿using Shooter.Game.Body;
+using Shooter.Game.Core;
+using Shooter.Logging;
+using UnityEngine;
+
+namespace Shooter.Game.World
+{
+    public class SpawnTeleport : MonoBehaviour, ITriggerable
+    {
+        private static readonly Journal Log = Logs.Here();
+
+        [SerializeField] private EarSoundSpec sound;
+
+        public void OnTrigger(Character character)
+        {
+            Movement movement = character.GetComponent<Movement>();
+            if (movement == null) return;
+
+            Sleeper sleeper = character.GetComponent<Sleeper>();
+
+            Vector3 destination = sleeper == null
+                ? (MainSpawnPoint.Current == null ? transform.position : MainSpawnPoint.Current.transform.position)
+                : sleeper.SpawnPoint;
+
+            Log.Info($"Entity {name} teleporting {movement.name} to their spawn point {destination}");
+            movement.Teleport(destination);
+            movement.GetComponent<EarSpeaker>()?.Play(sound);
+        }
+    }
+}
