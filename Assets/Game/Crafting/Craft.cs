c693cd5be34fa863cbfae85e6ecb33151e7cad83
mr2anderson <cheb0tarev.nikit0s@gmail.com>
2026-09-11 15:42:28 +0300

    add crafting core

---
+++ Assets/Game/Crafting/Craft.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Shooter.Game.Core;
+using Shooter.Game.Loot;
+using UnityEngine;
+
+namespace Shooter.Game.Crafting
+{
+    [CreateAssetMenu(menuName = "Shooter/Craft", fileName = "Craft")]
+    public class Craft : Spec
+    {
+        [SerializeField] private StackableItemSpec[] input = new StackableItemSpec[9];
+        [SerializeField] private StackableItemSpec output;
+
+        public StackableItemSpec[] Input => input;
+        public StackableItemSpec Output => output;
+
+        public Dictionary<StackableItemSpec, int> AmountMap()
+        {
+            var result = new Dictionary<StackableItemSpec, int>();
+            foreach (StackableItemSpec item in input)
+            {
+                result.TryAdd(item, 0);
+                result[item]++;
+            }
+            return result;
+        }
+    }
+}
---
+++ Assets/Game/Crafting/Crafter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using Shooter.Game.Loot;
+using Shooter.Logging;
+using UnityEngine;
+
+namespace Shooter.Game.Crafting
+{
+    [RequireComponent(typeof(Inventory))]
+    public class Crafter : MonoBehaviour
+    {
+        private static readonly Journal Log = Logs.Here();
+
+        [SerializeField] private List<Craft> availableCrafts;
+
+        private Inventory inventory;
+
+        private void Awake()
+        {
+            inventory = GetComponent<Inventory>();
+        }
+
+        public bool TryCraft(StackableItemSpec[] input)
+        {
+            if (input.Length != 9)
+            {
+                throw new ArgumentException("input length must be 9");
+            }
+
+            foreach (Craft craft in availableCrafts)
+            {
+                bool match = true;
+                for (int i = 0; i < 9; i++)
+                {
+                    if (craft.Input[i] != input[i])
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+
+                if (match)
+                {
+                    return TryCraft(craft);
+                }
+            }
+
+            return false;
+        }
+
+        private bool TryCraft(Craft craft)
+        {
+            Dictionary<StackableItemSpec, int> amountMap = craft.AmountMap();
+
+            foreach (var kvp in amountMap)
+            {
+                if (inventory.StackableAmount(kvp.Key) < kvp.Value)
+                {
+                    return false;
+                }
+            }
+
+            foreach (var kvp in amountMap)
+            {
+                inventory.RemoveStackable(kvp.Key, kvp.Value, InventoryOnConflict.Partly);
+            }
+
+            inventory.AddStackable(craft.Output, 1);
+            return true;
+        }
+    }
+}
