412a6383a3ce10d6c3b1ad1969867270faf4b72c
mr2anderson <cheb0tarev.nikit0s@gmail.com>
2026-09-15 23:36:49 +0300

    add groups

---
+++ Assets/Game/Core/Character/Groups/Group.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Shooter.Game.Core.Groups
+{
+    [CreateAssetMenu(menuName = "Shooter/Group", fileName = "Group")]
+    public class Group : Spec
+    {
+        [SerializeField] private string promptName;
+        public string PromptName => promptName;
+    }
+}
