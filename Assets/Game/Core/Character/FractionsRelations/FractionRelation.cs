6eb4c601cd53ecb33da69b8337212960288d7f25
mr2anderson <cheb0tarev.nikit0s@gmail.com>
2026-09-16 00:17:04 +0300

    add fraction relation system

---
+++ Assets/Game/Core/Character/FractionsRelations/FractionRelation.cs
@@ -0,0 +1,36 @@
+﻿using Shooter.Game.Core.Groups;
+using Shooter.Logging;
+using UnityEngine;
+
+namespace Shooter.Game.Core.FractionsRelations
+{
+    [CreateAssetMenu(menuName = "Shooter/Fraction Relation", fileName = "FractionRelation")]
+    public class FractionRelation : Spec
+    {
+        private static readonly Journal Log = Logs.Here();
+
+        [SerializeField] private Fraction fraction1;
+        [SerializeField] private Fraction fraction2;
+        [SerializeField] private int defaultAmount;
+
+        public Fraction Fraction1 => fraction1;
+        public Fraction Fraction2 => fraction2;
+        public int DefaultAmount => defaultAmount;
+
+        private void OnEnable()
+        {
+            if (fraction1 == null)
+            {
+                Log.Error($"Fraction relation {Id} does not have set fraction1");
+            }
+            if (fraction2 == null)
+            {
+                Log.Error($"Fraction relation {Id} does not have set fraction2");
+            }
+            if (defaultAmount < 0 || defaultAmount > 100)
+            {
+                Log.Error($"Fraction relation {Id} has bad default amount ({defaultAmount})");
+            }
+        }
+    }
+}
---
+++ Assets/Game/Core/Character/FractionsRelations/FractionRelationCatalog.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Shooter.Game.Core.Groups;
+using Shooter.Logging;
+using UnityEngine;
+
+namespace Shooter.Game.Core.FractionsRelations
+{
+    [CreateAssetMenu(menuName = "Shooter/Fraction Relation Catalog", fileName = "FractionRelationCatalog")]
+    public class FractionRelationCatalog : Catalog<FractionRelation>
+    {
+        private static readonly Journal Log = Logs.Here();
+
+        private readonly Dictionary<string, int> byFractions = new Dictionary<string, int>();
+
+        [SerializeField] private int defaultAmount = 50;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            for (int i = 0; i < Count; i++)
+            {
+                FractionRelation fractionRelation = At(i);
+                string key = KeyOf(fractionRelation.Fraction1.Id.ToString(), fractionRelation.Fraction2.Id.ToString());
+                if (!byFractions.TryAdd(key, fractionRelation.DefaultAmount))
+                {
+                    Log.Error($"Fraction relation {key} already has been set");
+                }
+            }
+        }
+
+        public int Amount(Fraction fraction1, Fraction fraction2)
+        {
+            string key = KeyOf(fraction1.Id.ToString(), fraction2.Id.ToString());
+            if (byFractions.TryGetValue(key, out int result))
+            {
+                return result;
+            }
+            Log.Error($"Failed to find relation value for {key}, falling back to {defaultAmount}");
+            return defaultAmount;
+        }
+
+        private static string KeyOf(string g1, string g2)
+        {
+            if (String.CompareOrdinal(g1, g2) > 0)
+            {
+                (g1, g2) = (g2, g1);
+            }
+            return g1 + "-" + g2;
+        }
+    }
+}
