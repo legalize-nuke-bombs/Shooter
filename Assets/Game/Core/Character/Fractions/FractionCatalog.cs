1b04755c2e21ed45a91f56afc31031d4b8ea1d0a
mr2anderson <cheb0tarev.nikit0s@gmail.com>
2026-09-15 23:53:00 +0300

    add fraction catalog

---
+++ Assets/Game/Core/Character/Fractions/FractionCatalog.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Shooter.Game.Core.Groups
+{
+    [CreateAssetMenu(menuName = "Shooter/Fraciton Catalog", fileName = "FractionCatalog")]
+    public class FractionCatalog : Catalog<Fraction>
+    {
+
+    }
+}
